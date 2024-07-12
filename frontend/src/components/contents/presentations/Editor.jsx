b6b1a0c7716c1bbf3ac098a2c8a378e7d624d641
Marco A S <aureliojuniorcmrj@hotmail.com>
2024-07-12 09:21:39 -0300

    Merge branch 'dev/create-edge-button' into testing-branch

--- frontend/src/components/contents/presentations/Editor.jsx
+++ frontend/src/components/contents/presentations/Editor.jsx
@@ -109,7 +109,7 @@ const Editor = ({
   return (
     <div className="container-fluid" style={{ padding: 0 }}>
       <div className="editor">
-        <div className="container-fluid editor-area card-header">
+        <div className="container-fluid editor-area card-header" style={{ paddingRight: 0 }}>
           <div className="input-group input-style">
             <div style={{
               height: '60px',
