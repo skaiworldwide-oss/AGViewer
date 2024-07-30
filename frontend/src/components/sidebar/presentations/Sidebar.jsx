ac0c34b06eda7f4a5599febbab2fb3a49b93107a
Marco A S <aureliojuniorcmrj@hotmail.com>
2024-07-30 10:26:39 -0300

    Merge branch 'feature/footer' into testing-branch

--- frontend/src/components/sidebar/presentations/Sidebar.jsx
+++ frontend/src/components/sidebar/presentations/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = ({ activeMenuName, isActive }) => (
   <div
     id="sidebar"
     className={isActive ? ' active ' : 'inactive'}
-    style={{ height: '100%' }}
+    style={{ height: '100%', margin: 0 }}
   >
     <div className="tab-content">
       <div className={`tab-pane fade${activeMenuName === 'home' ? ' active show ' : ''}`} role="tabpanel" aria-labelledby="side-home-tab">
