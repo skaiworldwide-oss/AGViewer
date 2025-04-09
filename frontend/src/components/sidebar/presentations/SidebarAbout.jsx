skaiworldwide-oss/AgensGraphViewer

Update README for SKAI Worldwide repository
Update README for SKAI Worldwide OSS repository link

frontend/src/components/sidebar/presentations/SidebarAbout.jsx
@@ -49,7 +49,7 @@ const SidebarAbout = ({ releaseDate, version, license }) => (
             label={(
               <span>
                 Leave your feedback at
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/bitnine-oss/AGViewer/issues"> Here!</a>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/skaiworldwide-oss/AgensGraphViewer/issues"> Here!</a>
                 {' '}
                 <br />
                 Your feedback help us provide you a better experience!

skaiworldwide-oss/AGViewer

frontend/src/components/sidebar/presentations/SidebarAbout.jsx
@@ -49,7 +49,7 @@ const SidebarAbout = ({ releaseDate, version, license }) => (
             label={(
               <span>
                 Leave your feedback at
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/bitnine-oss/AGViewer/issues"> Here!</a>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/skaiworldwide-oss/AGViewer/issues"> Here!</a>
                 {' '}
                 <br />
                 Your feedback help us provide you a better experience!