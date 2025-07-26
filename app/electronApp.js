f97715101d26e21ad3f9678dba4b428354f701ca
m-faseeh <muhammadfaseeh397@gmail.com>
2025-07-26 12:38:00 +0500

    add desktop environment build settings using Electron

---
+++ app/electronApp.js
@@ -0,0 +1,30 @@
+require('./resources/backend/build/bin/www');
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+    }
+  })
+
+  win.loadFile('./electron.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
