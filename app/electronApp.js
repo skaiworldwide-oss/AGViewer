6a82e4d805bf5d5c7ba86a69ea6b3a03ae9e86c1
m-faseeh <muhammadfaseeh397@gmail.com>
2025-08-28 23:54:41 +0500

    Include backend/sql in bundle to resolve missing query errors

--- app/electronApp.js
+++ app/electronApp.js
@@ -4,8 +4,8 @@ const path = require('path')
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
     }
   })
